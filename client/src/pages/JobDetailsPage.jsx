13b93276c4b3e27417397f8b3050a5c6a51deb4d
Simscope <sandrvlad@gmail.com>
2025-09-07 16:24:04 -0400

    Create JobDetailsPage.jsx

---
+++ client/src/pages/JobDetailsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function JobDetailsPage() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>Job Details</h1>
+      <p>Страница-заглушка. Заменим на реальный контент позже.</p>
+    </div>
+  );
+}
