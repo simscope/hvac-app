362fef89c99343c3ca71c4053e504f477906aec8
Simscope <sandrvlad@gmail.com>
2025-09-07 17:18:12 -0400

    Update supabaseClient.js

--- client/src/supabaseClient.js
+++ client/src/supabaseClient.js
@@ -1,6 +1,11 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  'https://jywvdftejvnisjvuidtt.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5d3ZkZnRlanZuaXNqdnVpZHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MjA5MzksImV4cCI6MjA2NjA5NjkzOX0._iZKkCzHcYUeU-37ZOJxYmvLgCFi0lIbR_xIbfK-EuA'
-)
+const url =
+  process.env.REACT_APP_SUPABASE_URL ||
+  process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+const anon =
+  process.env.REACT_APP_SUPABASE_ANON_KEY ||
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(url, anon);
