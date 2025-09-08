bf1829749df22da4c7cba540a14a9e94f5182cfe
Simscope <sandrvlad@gmail.com>
2025-09-07 20:26:12 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,17 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </HashRouter>
+  </React.StrictMode>
+);
