ede9458e899290a1f3be4998bd33bb7ec427dfaf
Simscope <sandrvlad@gmail.com>
2025-09-09 00:02:26 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
-// client/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './context/AuthContext'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </HashRouter>
 );
