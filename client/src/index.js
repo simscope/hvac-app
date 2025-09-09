21c619ee019b0c670724e3f8c5df8b29047a371d
Simscope <sandrvlad@gmail.com>
2025-09-08 22:27:16 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,8 @@
+// client/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
-
-function ErrorBoundary({ children }) {
-  const [err, setErr] = React.useState(null);
-  return err
-    ? <pre style={{whiteSpace:'pre-wrap',padding:16,color:'crimson'}}>Error: {String(err)}</pre>
-    : <React.ErrorBoundary fallbackRender={({error}) => <pre style={{whiteSpace:'pre-wrap',padding:16,color:'crimson'}}>{String(error)}</pre>}>{children}</React.ErrorBoundary>;
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
