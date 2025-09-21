2a7c2a02842db97b0beb1d2421ed8dcf34665f6a
Simscope <sandrvlad@gmail.com>
2025-09-21 14:09:44 -0400

    Update TopNav.jsx

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import NotificationsBell from './notifications/NotificationsBell.jsx';
+
 
 const NavItem = ({ to, label, active, badge }) => (
   <Link
