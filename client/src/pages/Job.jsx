135ce3cc2e6bf96afba09b473367d7caeb995183
Simscope <sandrvlad@gmail.com>
2025-10-06 10:46:22 -0400

    Update Job.jsx

--- client/src/pages/Job.jsx
+++ client/src/pages/Job.jsx
@@ -5,7 +5,7 @@ import JobList from '../components/JobList';
 export default function Job() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Управление заявками</h1>
+      <h1 className="text-2xl font-bold mb-4">Job Management</h1>
       <CreateJob />
       <JobList />
     </div>
